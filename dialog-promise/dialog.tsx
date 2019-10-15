43aedf1ea677f4fe6e92ddac75ed2b991a2f1821
libin40 <libin40@baidu.com>
2019-10-15 18:50:24 +0800

    a

---
+++ dialog-promise/dialog.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import './dialog.less';
+import { Transition } from 'react-transition-strict';
+interface Option {
+    opacity?: number
+    title?: string
+    warpClass?: string
+    content?: string
+    animation?: string   /* fade zoom slideDown slideLeft slideRight slideUp flip rotate door*/
+    btnSuccText?: string
+    onSucc?(event: any): any | Promise<any>
+    btnFailText?: string
+    onCancel?(event: any): any | Promise<any>
+    maskHide?: boolean
+    [name: string]: any
+}
+interface Props {
+    setting: Option,
+    [name: string]: any
+}
+export class DialogCom extends React.Component<Props, {}> {
+    mask: any;
+    constructor(props) {
+        super(props);
+        this.mask = '';
+    }
+    componentDidMount() {
+        this.mask.addEventListener('touchmove', function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+        }, false);
+    }
+    onCancel = (e) => {
+        e.stopPropagation();
+        const {
+            onCancel,
+        } = this.props.setting;
+        const { context } = this.props;
+        const cancelVal = onCancel(e);
+        if (Object.prototype.toString.call(cancelVal) == '[object Promise]') {
+            cancelVal.then(() => {
+                context.hide();
+            });
+        } else {
+            context.hide();
+        }
+    }
+    onSucc = (e) => {
+        e.stopPropagation();
+        const {
+            onSucc,
+        } = this.props.setting;
+        const { context } = this.props;
+        const succVal = onSucc(e);
+        if (Object.prototype.toString.call(succVal) == '[object Promise]') {
+            succVal.then(() => {
+                context.hide();
+            });
+        } else {
+            context.hide();
+        }
+    }
+    render() {
+        const {
+            opacity,
+            title,
+            content,
+            btnSuccText,
+            btnFailText,
+            maskHide,
+            animation,
+            warpClass
+        } = this.props.setting;
+        const { context } = this.props;
+        const style = {
+            'background': `rgba(0,0,0,${opacity})`
+        };
+        return (
+            <div className='hk-mask' ref={(el) => { this.mask = el; }} onClick={(e) => {
+                e.stopPropagation();
+                maskHide && context.hide();
+            }} style={style}>
+                <Transition animation={animation}>
+                    <div className={`hk-dialog ${warpClass}`}>
+                        {title && <div className='hk-title1'>{title}</div>}
+                        <p className='hk-title1 hk-title2'>{content}</p>
+                        <div className='hk-btnlist onepx-top-border'>
+                            {btnFailText && <div className='hk-abort' onClick={this.onCancel}>{btnFailText}</div>}
+                            <div className='hk-ok onepx-left-border' onClick={this.onSucc}>{btnSuccText}</div>
+                        </div>
+                    </div>
+                </Transition>
+            </div >
+        );
+    }
+}
---
+++ dialog-promise/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { DialogCom } from './dialog';
+interface Option {
+    opacity?: number
+    title?: string
+    warpClass?: string
+    content?: string
+    animation?: string   /* fade zoom slideDown slideLeft slideRight slideUp flip rotate door*/
+    btnSuccText?: string
+    onSucc?(event: any): any | Promise<any>
+    btnFailText?: string
+    onCancel?(event: any): any | Promise<any>
+    maskHide?: boolean
+    [name: string]: any
+}
+export class Dialog {
+    static dialogEle = '';
+    static show(option: Option) {
+        const setting: Option = {
+            opacity: 0.6,
+            title: '提示信息',
+            content: '消息提示成功',
+            animation: 'slideDown',   /* fade zoom slideDown slideLeft slideRight slideUp flip rotate door */
+            btnSuccText: '确定',
+            onSucc() { },
+            btnFailText: '',
+            onCancel() { },
+            maskHide: false,
+            warpClass: ''
+        };
+        this.dialog({
+            ...setting,
+            ...option
+        });
+    }
+    static dialog(setting: Option) {
+        const div = document.createElement('div');
+        const id = document.createAttribute('id');
+        this.dialogEle = 'dialogEle-' + new Date().getTime();
+        id.value = this.dialogEle;
+        div.setAttributeNode(id);
+        document.body.appendChild(div);
+        ReactDOM.render(<DialogCom setting={setting} context={Dialog} />, div);
+    }
+    static hide() {
+        const dialogEle = document.querySelector('#' + this.dialogEle);
+        if (dialogEle) {
+            ReactDOM.unmountComponentAtNode(dialogEle);
+            document.body.removeChild(dialogEle);
+        }
+    }
+}
